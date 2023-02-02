9a8df51e299536fd3ad5331ad794670fa97b808e
selee <tldms296@gmail.com>
2023-02-02 12:56:25 +0900

    그림판 update

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,8 +1,105 @@
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOptions = Array.from(
+    document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+
+let isPainting = false;
+let isFilling = false;
+
+const colors = [
+    "#f03e3e",
+    "#e64980",
+    "#be4bdb",
+    "#7950f2",
+    "#4c6ef5",
+    "#228be6",
+    "#15aabf",
+    "#12b886",
+    "#40c057",
+    "#82c91e",
+    "#fab005",
+    "#fd7e14"
+]
+
+function onMove(event) {
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+    isPainting = true;
+}
+function cancelPainting(){
+    isPainting = false;
+}
+function onLineWidthChange(event) {
+    ctx.beginPath();
+    ctx.lineWidth = event.target.value;
+}
+function onColorChange(event) {
+    ctx.beginPath();
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+function onColorClick(event) {
+    ctx.beginPath();
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+function onModeClick() {
+    if(isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+function onCanvasClick() {
+    if(isFilling) {
+        // ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+        ctx.fill();
+    }
+}
+function onDestroyClick() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+function onEraserClick() {
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
 
 /*
  * 사각형 만들고 채우는 가장 간단한 방법
@@ -45,6 +142,8 @@ ctx.lineTo(450, 200);
 ctx.fill();
 */
 
+/*
+ * 로봇 만들기 
 ctx.fillRect(170, 160, 15, 100);
 ctx.fillRect(310, 160, 15, 100);
 ctx.fillRect(220, 160, 60, 200);
@@ -66,7 +165,5 @@ ctx.beginPath();
 ctx.fillStyle = "red";
 ctx.arc(250, 110, 16, 0, Math.PI);
 ctx.fill();
-
-
-
+*/
 
